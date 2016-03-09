1871284e2013f993dce95797e20869521d6f236f
flowerwrong <yangkang@liveneeq.com>
2016-03-09 19:01:46 +0800

    Update demux add encoder and decoder

--- src/demux.cpp
+++ src/demux.cpp
@@ -2,22 +2,19 @@
 
 #define __STDC_CONSTANT_MACROS
 
-#ifdef _WIN32
-// Windows
-extern "C"
-{
-#include "libavformat/avformat.h"
-};
-#else
 // Linux...
 #ifdef __cplusplus
 extern "C" {
 #endif
+#include <libavutil/imgutils.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/timestamp.h>
+#include <libavutil/opt.h>
 #include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
 #ifdef __cplusplus
 }
 ;
-#endif
 #endif
 
 /*
@@ -28,23 +25,34 @@ extern "C" {
  H.264 in some container (MPEG2TS) don't need this BSF.
  */
 
+// 将一个视频先demux，然后解码再编码为 output.h264 and output.aac
+int demux(const char *in_filename, const char *out_filename_v,
+		const char *out_filename_a);
+
 //'1': Use H.264 Bitstream Filter
 #define USE_H264BSF 0
 
 int main(int argc, char* argv[]) {
+	if (argc < 4) {
+		printf("missing some argv\n");
+		return -1;
+	}
+	const char *in_filename = argv[1];
+	const char *out_filename_v = argv[2];
+	const char *out_filename_a = argv[3];
+	int ret = demux(in_filename, out_filename_v, out_filename_a);
+	return 0;
+}
+
+int demux(const char *in_filename, const char *out_filename_v,
+		const char *out_filename_a) {
 	AVOutputFormat *ofmt_a = NULL, *ofmt_v = NULL;
 	// Input AVFormatContext and Output AVFormatContext
 	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx_a = NULL, *ofmt_ctx_v = NULL;
-	AVPacket pkt;
+	AVPacket pkt, enc_pkt;
 	int ret, i;
 	int video_index = -1, audio_index = -1;
 	int frame_index = 0;
-
-	const char *in_filename = "cuc_ieschool.ts"; // Input file URL
-	//char *in_filename  = "cuc_ieschool.mkv";
-	const char *out_filename_v = "cuc_ieschool.h264"; // Output file URL
-	//char *out_filename_a = "cuc_ieschool.mp3";
-	const char *out_filename_a = "cuc_ieschool.aac";
 
 	av_register_all();
 	// Input
@@ -112,14 +120,6 @@ int main(int argc, char* argv[]) {
 			out_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
 	}
 
-	// Dump Format------------------
-	printf("\n==============Input Video=============\n");
-	av_dump_format(ifmt_ctx, 0, in_filename, 0);
-	printf("\n==============Output Video============\n");
-	av_dump_format(ofmt_ctx_v, 0, out_filename_v, 1);
-	printf("\n==============Output Audio============\n");
-	av_dump_format(ofmt_ctx_a, 0, out_filename_a, 1);
-	printf("\n======================================\n");
 	// Open output file
 	if (!(ofmt_v->flags & AVFMT_NOFILE)) {
 		if (avio_open(&ofmt_ctx_v->pb, out_filename_v, AVIO_FLAG_WRITE) < 0) {
@@ -140,10 +140,10 @@ int main(int argc, char* argv[]) {
 		printf("Error occurred when opening video output file\n");
 		goto end;
 	}
-	if (avformat_write_header(ofmt_ctx_a, NULL) < 0) {
-		printf("Error occurred when opening audio output file\n");
-		goto end;
-	}
+//	if (avformat_write_header(ofmt_ctx_a, NULL) < 0) {
+//		printf("Error occurred when opening audio output file\n");
+//		goto end;
+//	}
 
 #if USE_H264BSF
 	AVBitStreamFilterContext* h264bsfc = av_bitstream_filter_init("h264_mp4toannexb");
@@ -152,45 +152,124 @@ int main(int argc, char* argv[]) {
 	while (1) {
 		AVFormatContext *ofmt_ctx;
 		AVStream *in_stream, *out_stream;
+
+		AVCodecContext *dec_ctx = NULL, *enc_ctx = NULL;
+		AVCodec *dec = NULL, *encoder = NULL;
+
+		AVFrame *frame = NULL;
+
+		int got_frame;
+
 		// Get an AVPacket
 		if (av_read_frame(ifmt_ctx, &pkt) < 0)
 			break;
 		in_stream = ifmt_ctx->streams[pkt.stream_index];
 
 		if (pkt.stream_index == video_index) {
-			out_stream = ofmt_ctx_v->streams[0];
 			ofmt_ctx = ofmt_ctx_v;
-			printf("Write Video Packet. size:%d\tpts:%lld\n", pkt.size,
-					pkt.pts);
+			out_stream = avformat_new_stream(ofmt_ctx, NULL);
+
+			/* find decoder for the stream */
+			dec_ctx = in_stream->codec;
+			dec = avcodec_find_decoder(dec_ctx->codec_id);
+			if (!dec) {
+				fprintf(stderr, "Failed to find %s codec\n",
+						av_get_media_type_string(AVMEDIA_TYPE_VIDEO));
+				return AVERROR(EINVAL);
+			}
+
+			/* Open decoder */
+			int ret = avcodec_open2(dec_ctx, dec, NULL);
+			if (ret < 0) {
+				av_log(NULL, AV_LOG_ERROR,
+						"Failed to open decoder for stream #%u\n", i);
+				return ret;
+			}
+
+			// decoder is MPEG-4 part 2
+			printf("decoder is %s\n", dec->long_name);
+
+			// NOTE
+			frame = av_frame_alloc();
+
+			ret = avcodec_decode_video2(dec_ctx, frame, &got_frame, &pkt);
+			if (ret < 0) {
+				av_frame_free(&frame);
+				av_log(NULL, AV_LOG_ERROR, "Decoding failed\n");
+				break;
+			}
+
+			// printf("frame duration is %d\n", frame->pkt_duration);
+
+			// encode
+			AVCodecID h264 = AV_CODEC_ID_H264;
+			encoder = avcodec_find_encoder(h264);
+
+			// avcodec_copy_context(enc_ctx, dec_ctx);
+			enc_ctx = avcodec_alloc_context3(encoder);
+            if (!encoder) {
+                av_log(NULL, AV_LOG_FATAL, "Necessary encoder not found\n");
+                return AVERROR_INVALIDDATA;
+            }
+			if (dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO) {
+				enc_ctx->height = dec_ctx->height;
+				enc_ctx->width = dec_ctx->width;
+				enc_ctx->sample_aspect_ratio = dec_ctx->sample_aspect_ratio;
+				/* take first format from list of supported formats */
+				enc_ctx->pix_fmt = encoder->pix_fmts[0];
+				/* video time_base can be set to whatever is handy and supported by encoder */
+				enc_ctx->time_base = dec_ctx->time_base;
+			}
+
+		    av_opt_set(enc_ctx->priv_data, "preset", "slow", 0);
+
+			// AVOutputFormat *formatOut = av_guess_format(NULL, out_filename_v, NULL);
+
+			enc_pkt.data = NULL;
+			enc_pkt.size = 0;
+			av_init_packet(&enc_pkt);
+			ret = avcodec_open2(enc_ctx, encoder, NULL);
+			if (ret < 0) {
+				av_log(NULL, AV_LOG_ERROR, "Failed to open encoder for stream #%u\n", i);
+				return ret;
+			}
+		    frame->format = enc_ctx->pix_fmt;
+		    frame->width  = enc_ctx->width;
+		    frame->height = enc_ctx->height;
+			ret = avcodec_encode_video2(enc_ctx, &enc_pkt, frame, &got_frame);
+
+			printf("demo is %s\n", "hello");
+
+
+			av_frame_free(&frame);
+			avcodec_close(enc_ctx);
+			avcodec_close(dec_ctx);
+
+			// printf("Write Video Packet. size:%d\tpts:%lld\n", pkt.size, pkt.pts);
 #if USE_H264BSF
 			av_bitstream_filter_filter(h264bsfc, in_stream->codec, NULL, &pkt.data, &pkt.size, pkt.data, pkt.size, 0);
 #endif
-		} else if (pkt.stream_index == audio_index) {
-			out_stream = ofmt_ctx_a->streams[0];
-			ofmt_ctx = ofmt_ctx_a;
-			printf("Write Audio Packet. size:%d\tpts:%lld\n", pkt.size,
-					pkt.pts);
 		} else {
 			continue;
 		}
 
 		// Convert PTS/DTS
-		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base,
+		enc_pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base,
 				out_stream->time_base,
 				(AVRounding) (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base,
+		enc_pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base,
 				out_stream->time_base,
 				(AVRounding) (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base,
+		enc_pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base,
 				out_stream->time_base);
-		pkt.pos = -1;
-		pkt.stream_index = 0;
-		// Write
-		if (av_interleaved_write_frame(ofmt_ctx, &pkt) < 0) {
+		enc_pkt.pos = -1;
+		enc_pkt.stream_index = 0;
+
+		if (av_interleaved_write_frame(ofmt_ctx, &enc_pkt) < 0) {
 			printf("Error muxing packet\n");
 			break;
 		}
-		// printf("Write %8d frames to output file\n",frame_index);
+		av_free_packet(&enc_pkt);
 		av_free_packet(&pkt);
 		frame_index++;
 	}
@@ -202,8 +281,8 @@ int main(int argc, char* argv[]) {
 	// Write file trailer
 	av_write_trailer(ofmt_ctx_a);
 	av_write_trailer(ofmt_ctx_v);
-end:
-	avformat_close_input(&ifmt_ctx);
+
+	end: avformat_close_input(&ifmt_ctx);
 	/* close output */
 	if (ofmt_ctx_a && !(ofmt_a->flags & AVFMT_NOFILE))
 		avio_close(ofmt_ctx_a->pb);
